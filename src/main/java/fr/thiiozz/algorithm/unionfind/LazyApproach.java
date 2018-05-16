a3b4ca3562bc281ad609b5267dcee5099c5a5b10
theo.vermersch <theo.vermersch@ext-cdiscount.com>
2018-05-16 17:15:35 +0200

    add uf lazy approach

--- src/main/java/fr/thiiozz/algorithm/unionfind/LazyApproach.java
+++ src/main/java/fr/thiiozz/algorithm/unionfind/LazyApproach.java
@@ -1,4 +1,24 @@
 package fr.thiiozz.algorithm.unionfind;
 
-public class LazyApproach {
+public class LazyApproach extends AbstractUF {
+    public LazyApproach(int N) {
+        super(N);
+    }
+
+    @Override
+    public void union(int p, int q) {
+       int i = root(p);
+       int j = root(q);
+
+       id[i] = j;
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    private int root(int i) {
+        return (i == id[i]) ? i : root(id[i]);
+    }
 }
--- src/test/java/fr/thiiozz/algorithm/unionfind/LazyApproachTest.java
+++ src/test/java/fr/thiiozz/algorithm/unionfind/LazyApproachTest.java
@@ -6,12 +6,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class DumbApproachTest extends AbstractUFTest {
+public class LazyApproachTest extends AbstractUFTest{
     @Before
     public void setUp() {
-        uf = new DumbApproach(NUMBER_OF_ELEMENTS);
+        uf = new LazyApproach(NUMBER_OF_ELEMENTS);
     }
-
 
     @Test
     public void initialNoElementsAreUnited() {
