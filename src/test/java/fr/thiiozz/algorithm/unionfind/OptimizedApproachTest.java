1782e0c2c8fac116def192920a1984fa7d1e4009
theo.vermersch <theo.vermersch@ext-cdiscount.com>
2018-05-16 17:42:38 +0200

    Optimized impl

---
+++ src/test/java/fr/thiiozz/algorithm/unionfind/OptimizedApproachTest.java
@@ -0,0 +1,52 @@
+package fr.thiiozz.algorithm.unionfind;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class OptimizedApproachTest extends AbstractUFTest {
+    @Before
+    public void setUp() {
+        uf = new OptimizedApproach(NUMBER_OF_ELEMENTS);
+    }
+
+
+    @Test
+    public void initialNoElementsAreUnited() {
+        for (int i = 0; i < NUMBER_OF_ELEMENTS; i++)
+            assertTrue(uf.getId()[i] == i);
+    }
+
+    @Test
+    public void twoElementsAreInitialNotUnited() {
+        unions.clear();
+        unions.add("0,1");
+        unions.add("1,2");
+        connect();
+
+        assertFalse(uf.connected(4, 3));
+    }
+
+    @Test
+    public void twoElementsCanBeUnited() {
+        unions.clear();
+        unions.add("4,3");
+        connect();
+
+        assertTrue(uf.connected(4, 3));
+    }
+
+    @Test
+    public void multipleElementsCanBeUnited() {
+        unions.clear();
+        unions.add("4,3");
+        unions.add("3,8");
+        unions.add("8,9");
+        unions.add("9,0");
+        connect();
+
+        assertTrue(uf.connected(9, 0));
+    }
+}
